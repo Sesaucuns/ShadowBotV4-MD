4d27be76d14c5f9bcfad2483b1dd1c0bd76a6df2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-08 00:12:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,18 +8,8 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['573225236629', '💫 Mystic - Collaborator 4 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 5 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 6 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 7 💫', true],	
-  ['593968585283'],
-  ['5219993404349'],
-  //['593968585383'],	
-  ['5219996125657']
+  ['593959425714', '👑 DIEGO-OFC 👑', true]
+  ['5219992095479', '👑 BRUNO-SOBRINO', true]
 ] 
 
 global.suittag = ['5219993404349'] 
