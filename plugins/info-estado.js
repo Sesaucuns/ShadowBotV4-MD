d2966c6b0eb2d906a478a35cbf5750da5342ba45
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-09 15:08:52 -0400

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -3,7 +3,13 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 conn.sendButton(m.chat, `ミ💖 Hola estimado usuario 💖彡*
 *ミ🤖 Estado del Bot 🤖彡*
 *=> Bot activo ✔️*
-*=> Bot uso público ✔️*`.trim(), wm, [['🔷️ MENÚ 🔷', `#menu`]],m)}
+*=> Bot uso público ✔️*`.trim(), wm,
+[
+['🔷️ MENÚ 🔷', `#menu`],
+['🔶️ MENÚ SIMPLE 🔶️',`menusimple`],
+ ['💠️ MENÚ AUDIOS 💠️', `audios`]
+],m)}
+
 
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 
