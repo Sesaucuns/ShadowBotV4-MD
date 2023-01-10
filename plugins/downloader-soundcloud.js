a5ed42e6ae7123835f43c9537c548535129de0ca
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-10 18:22:15 -0400

    Update downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+if (!text) throw `*✳️ Ingresa el nombre de alguna canción*`
 try {
 let res = await fetch(`https://api.akuari.my.id/search/searchsoundcloud?query=${text}`)
 let json2 = await res.json()
@@ -10,13 +10,13 @@ let json = await res2.json()
 let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${json.link}`)).text()
 let soundcloudt = `❒═══❬ 𝐒𝐎𝐔𝐍𝐃𝐂𝐋𝐎𝐔𝐃 ❭═══╾❒
 ┬
-├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${json.title}
+├‣✨ *TÍTULO:* ${json.title}
 ┴
 ┬
-├‣💚 *𝚄𝚁𝙻 𝙳𝙸𝚁𝙴𝙲𝚃𝙾:* ${shortUrl}
+├‣💚 *URL DIRECTO:* ${shortUrl}
 ┴
 ┬
-├‣ *- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚖𝚞𝚜𝚒𝚌𝚊...*
+├‣ *-⏰ ENVIANDO MUSICA...*
 ┴
 ┬
 ├ _﹫ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ_
@@ -25,7 +25,7 @@ conn.sendFile(m.chat, json.thumb, '', soundcloudt, m)
 conn.sendMessage(m.chat, { audio: { url: json.link }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
 //conn.sendFile(m.chat, json.link, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })
 } catch (e) {
-throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙻𝙾𝙶𝚁𝙾 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*'
+throw '*⚠️ No se encontró la canción o la página está caída*'
 }}
 handler.command = /^(soundcloud|cover)$/i
 export default handler
