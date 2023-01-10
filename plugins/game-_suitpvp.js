03c82ffc56dad1ed147620d5dec51f38609c16f1
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-10 19:54:32 -0400

    Update game-_suitpvp.js

--- plugins/game-_suitpvp.js
+++ plugins/game-_suitpvp.js
@@ -18,13 +18,13 @@ clearTimeout(room.waktu)
 let textplay = `🎮 GAMES - PVP - GAMES 🎮\n\n—◉ EL JUEGO COMIENZA, LAS OPCIONES HAN SIDO ENVIADAS  A LOS CHATS PRIVADOS DE @${room.p.split`@`[0]} 𝚈 @${room.p2.split`@`[0]}\n\n◉ SELECCIONEN UNA OPCION EN SUS CHATS PRIVADOS, RESPECTIVAMENTE\n*◉ ELEGIR OPCIÓN EN wa.me/${conn.user.jid.split`@`[0]}*`
 m.reply(textplay, m.chat, {mentions: this.parseMention(textplay)})
 let imgplay = `https://www.merca2.es/wp-content/uploads/2020/05/Piedra-papel-o-tijera-0003318_1584-825x259.jpeg`    
-if (!room.pilih) this.sendHydrated(room.p, '𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙴 𝚄𝙽𝙰 𝙳𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂', `GANADOR +${room.poin}𝚇𝙿\nPERDEDOR ${room.poin_lose}𝚇𝙿`, imgplay, null, null, null, null, [['PIEDRA 🗿', 'Piedra'], ['PAPEL 📄', 'Papel'], ['TIJERA ✂️', 'Tijera']], m)
-if (!room.pilih2) this.sendHydrated(room.p2, '𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙴 𝚄𝙽𝙰 𝙳𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂', `GANADOR +${room.poin}𝚇𝙿\nPERDEDOR ${room.poin_lose}𝚇𝙿`, imgplay, null, null, null, null, [['PIEDRA 🗿', 'Piedra'], ['PAPEL 📄', 'Papel'], ['TIJERA ✂️', 'Tijera']], m)                             
+if (!room.pilih) this.sendHydrated(room.p, 'POR FAVOR SELECCIONE UNA DE LAS SIGUIENTES OPCIONES', `GANADOR +${room.poin}𝚇𝙿\nPERDEDOR ${room.poin_lose}𝚇𝙿`, imgplay, null, null, null, null, [['PIEDRA 🗿', 'Piedra'], ['PAPEL 📄', 'Papel'], ['TIJERA ✂️', 'Tijera']], m)
+if (!room.pilih2) this.sendHydrated(room.p2, 'POR FAVOR SELECCIONE UNA DE LAS SIGUIENTES OPCIONES', `GANADOR +${room.poin}𝚇𝙿\nPERDEDOR ${room.poin_lose}𝚇𝙿`, imgplay, null, null, null, null, [['PIEDRA 🗿', 'Piedra'], ['PAPEL 📄', 'Papel'], ['TIJERA ✂️', 'Tijera']], m)                             
 room.waktu_milih = setTimeout(() => {
-if (!room.pilih && !room.pilih2) this.sendButton(m.chat, `[❗] 𝙽𝙸𝙽𝙶𝚄𝙽 𝙹𝚄𝙶𝙰𝙳𝙾𝚁 𝚃𝙾𝙼𝙾 𝙻𝙰 𝙸𝙽𝙸𝙲𝙸𝙰𝚃𝙸𝚅𝙰 𝙳𝙴 𝙴𝙼𝙴𝙿𝙴𝚉𝙰𝚁 𝙴𝙻 𝙹𝚄𝙴𝙶𝙾, 𝙴𝙻 𝙿𝚅𝙿 𝚂𝙴 𝙰𝙷 𝙲𝙰𝙽𝙲𝙴𝙻𝙰𝙳𝙾`, wm, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '#menu']], m)
+if (!room.pilih && !room.pilih2) this.sendButton(m.chat, `⚠️ NINGUN JUGADOR TOMO LA INICIO DE INICIAR EL JUEGO, EL PVP SE A CANCELADO`, wm, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '#menu']], m)
 else if (!room.pilih || !room.pilih2) {
 win = !room.pilih ? room.p2 : room.p 
-let textnull = `*[❗] @${(room.pilih ? room.p2 : room.p).split`@`[0]} 𝙽𝙾 𝙴𝙻𝙴𝙶𝙸𝚂𝚃𝙴 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙾𝙿𝙲𝙸𝙾𝙽, 𝙵𝙸𝙽 𝙳𝙴𝙻 𝙿𝚅𝙿*`
+let textnull = `*⚠️ @${(room.pilih ? room.p2 : room.p).split`@`[0]} NO ELEGISTE NINGUNA OPCIÓN, FIN DEL PVP*`
 this.sendButton(m.chat, textnull, wm, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '#menu']], m, { mentions: this.parseMention(textnull)})
 db.data.users[win == room.p ? room.p : room.p2].exp += room.poin
 db.data.users[win == room.p ? room.p : room.p2].exp += room.poin_bot
