40ac6e669ec47cf6c8cc6a99d89e7ce8267e28cf
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-10 17:42:10 -0400

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -4,8 +4,8 @@ if (!text) throw `*[❗] Ejemplo de uso: ${usedPrefix + command} Minecraft*`
 const res = await googleImage(text)
 let image = await res.getRandom()
 let link = image
-let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
-conn.sendButton(m.chat, captionn, author, link, [['🔄 SIGUIENTE 🔄', `#imagen ${text}`]], m)}
+let captionn = `🔎 *RESULTADO DE:* ${text}\n🔗 *LINK:* ${link}\n🌎 *BUSCADOR:* Google`
+conn.sendButton(m.chat, captionn, wm, link, [['🔄 SIGUIENTE 🔄', `#imagen ${text}`]], m)}
 handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
 handler.command = /^(gimage|image|imagen)$/i
