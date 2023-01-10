b9c13c384d58375774264d602eabb0d18791e247
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-10 18:27:05 -0400

    Update downloader-stickerpack.js

--- plugins/downloader-stickerpack.js
+++ plugins/downloader-stickerpack.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`
+if (!text) throw `*✳️ EJEMPLO DE USO DEL COMANDO ${usedPrefix + command}* https://getstickerpack.com/stickers/flork-memes-4-1`
 try {
 let url = text
 let res = await fetch(`https://api.akuari.my.id/downloader/stickerpack?link=${url}`)
@@ -12,7 +12,7 @@ const stikers = await sticker(false, data, global.packname, global.author)
 conn.sendFile(m.chat, stikers, null, { asSticker: true }, m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': true }}, { quoted: m })
 //await delay(1500)
 }} catch {   
-await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*')  
+await m.reply('*⚠️ Error inténtalo de nuevo.*')  
 }}
 handler.command = /^stickerpack$/i
 export default handler
