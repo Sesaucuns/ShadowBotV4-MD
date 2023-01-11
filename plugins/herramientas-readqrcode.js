ed61c828d466787a5605d562352a6fa3ed164089
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-10 23:33:51 -0400

    Update herramientas-readqrcode.js

--- plugins/herramientas-readqrcode.js
+++ plugins/herramientas-readqrcode.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 / 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*'
+if (!mime) throw '*⚠️ ETIQUETE UNA IMAGEN CON EL COMANDO*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
