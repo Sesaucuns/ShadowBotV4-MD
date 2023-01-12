de36b8ea94473f0f644f47e3fa0a956ff56cc5fa
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 13:30:04 -0400

    Update owner-addmsg.js.js

--- plugins/owner-addmsg.js.js
+++ plugins/owner-addmsg.js.js
@@ -6,7 +6,7 @@ let handler = async (m, { command, usedPrefix, text }) => {
     let msgs = global.db.data.msgs
     if (text in msgs) throw `*✅ '${text}' FUE AÑADIDO A LA LISTA DE MENSAJES`
     msgs[text] = M.toObject(await m.getQuotedObj())
-    m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙰𝙶𝚁𝙴𝙶𝙰𝙳𝙾 𝙴𝚇𝙸𝚃𝙾𝚂𝙰𝙼𝙴𝙽𝚃𝙴 𝙰 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙲𝙾𝙼𝙾 '${text}'*\n*𝙰𝙲𝙲𝙴𝙳𝙴 𝙲𝙾𝙽 ${usedPrefix}ver${which} ${text}*`)
+    m.reply(`*✅ Mensaje agregado a la lista como '${text}'*\n*ACCEDE CON ${usedPrefix}ver${which} ${text}*`)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <text>')
 handler.tags = ['database']
