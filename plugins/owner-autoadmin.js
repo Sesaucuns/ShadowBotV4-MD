cb5a38557a9830f349de69c018abe7b8b5570472
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 02:45:32 -0400

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -2,11 +2,11 @@
 
 let handler = async (m, { conn, isAdmin }) => {  
 if (m.fromMe) return
-if (isAdmin) throw '*[❗] 𝙷𝙾𝙻𝙰 𝙲𝚁𝙴𝙰𝙳𝙾𝚁, 𝙲𝙾𝙼𝙾 𝙴𝚂𝚃𝙰? 𝚄𝚂𝚃𝙴𝙳 𝚈𝙰 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*'
+if (isAdmin) throw '*⚠️ HOLA CREADOR, USTED YA ES ADMIN*'
 try {  
 await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 } catch {
-await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙰𝚁𝙻𝙴 𝙰𝙳𝙼𝙸𝙽*')}}
+await m.reply('*⚠️ error, no pude darte admin*')}}
 handler.command = /^autoadmin$/i
 handler.rowner = true
 handler.group = true
