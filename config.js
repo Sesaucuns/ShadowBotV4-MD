81dec454746c48b1f97a5a6a2ef45ee32e6a37da
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 19:06:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -54,11 +54,11 @@ global.APIKeys = {
   'https://zenzapis.xyz': `${keysxxx}`,
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
-
+============== IMAGEN ============
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
-global.imagen4 = fs.readFileSync('./Menu.png')
+global.imagen4 = fs.readFileSync('./Menu2.png')
 
 global.mods = [] 
 global.flaaa = [
@@ -67,7 +67,7 @@ global.flaaa = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-//********Tiempo***************
+//=============Tiempo=============
 global.d = new Date(new Date + 3600000)
 global.locale = 'es'
 global.dia = d.toLocaleDateString(locale, { weekday: 'long' })
@@ -75,7 +75,8 @@ global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', ye
 global.mes = d.toLocaleDateString('es', { month: 'long' })
 global.año = d.toLocaleDateString('es', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
-//*****************************
+
+//=============RPG=============
 global.wm2 = `The Shadow Brokers - Bot - MD`
 global.gt = 'The Shadow Brokers - Bot'
 global.shadowbot = 'The Shadow Brokers - Bot'
@@ -102,10 +103,7 @@ global.fin = '════◕◕ • •'
 global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}` //Asia/Jakarta
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`//America/Los_Angeles
 global.fgif = {key: { participant : '0@s.whatsapp.net'}, message: { "videoMessage": { "title": wm, "h": `Hmm`, 'seconds': '999999999',  'gifPlayback': 'true',  'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu2.jpg')}}}
-global.multiplier = 99
-//*************************
-
-/*************************/
+global.multiplier = 69                  
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
@@ -451,7 +449,9 @@ let results = Object.keys(emotttt).map(v => [v, new RegExp(v, 'gi')]).filter(v =
 if (!results.length) return ''
 else return emotttt[results[0][0]]
 }}	
+//==========================
 
+//=============NO TOCAR=============
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
 unwatchFile(file)
