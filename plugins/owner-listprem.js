fae58dcded609fc8e929bb8426254f0c70caec5c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 02:14:29 -0400

    Update owner-listprem.js

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-let textprem = `*「 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 」*
+let textprem = `*「 USUARIOS PREMIUM 」*
 ` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 m.reply(textprem, null, {mentions: conn.parseMention(textprem)})
 }
