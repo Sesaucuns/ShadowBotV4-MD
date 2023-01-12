358038e4a83bff7abe5acfb873d2663c0d74c069
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 02:16:32 -0400

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*') 
+await conn.reply(id, '*Adios, ShadowBot se despide!*') 
 await conn.groupLeave(id)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
