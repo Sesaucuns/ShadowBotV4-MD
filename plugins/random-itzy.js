0dce3f5a6b6d788cc530a09c86098b8823a59b3f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 01:27:02 -0400

    Update random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/itzy.json`)).data  
-let mystic = await res[Math.floor(res.length * Math.random())]
-conn.sendButton(m.chat, `_${command}_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
+let res = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/ShadowBotV4-MD/master/src/JSON/itzy.json`)).data  
+let Shadow = await res[Math.floor(res.length * Math.random())]
+conn.sendButton(m.chat, `_${command}_`, author, Shadow, [['🔄 SIGUIENTE 🔄', `/${command}`]], m)}
 handler.help = ['itzy','kpopitzy']
 handler.tags = ['internet']
 handler.command = /^(itzy|kpopitzy)$/i
