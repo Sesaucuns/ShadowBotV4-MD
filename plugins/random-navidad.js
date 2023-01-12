ff0fb7f34caabf86173dec23c5060730b311e0cb
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 01:22:53 -0400

    Update random-navidad.js

--- plugins/random-navidad.js
+++ plugins/random-navidad.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/ShadowBotV4-MD/master/src/JSON/navidad.json`)).data  
-let mystic = await res[Math.floor(res.length * Math.random())]
-conn.sendButton(m.chat, `_Navidad 🧑‍🎄_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)}
+let shadow = await res[Math.floor(res.length * Math.random())]
+conn.sendButton(m.chat, `_Navidad 🧑‍🎄_`, author, shadow, [['🔄 SIGUIENTE 🔄', `${usedPrefix + command}`]], m)}
 handler.help = ['navidad']
 handler.tags = ['internet']
 handler.command = /^(navidad)$/i
