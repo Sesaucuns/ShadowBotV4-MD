b99b798b101f82940b7dc219f5c8251abe0606e6
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-12 02:25:59 -0400

    Update owner-delmsg.js

--- plugins/owner-delmsg.js
+++ plugins/owner-delmsg.js
@@ -1,10 +1,10 @@
 let handler = async (m, { command, usedPrefix, text }) => {
 let which = command.replace(/eliminar/i, '')
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙰𝚁 ${usedPrefix}list${which} 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰*`
+if (!text) throw `*⚠️ USAR ${usedPrefix}list${which} PARA VER LA LISTA*`
 let msgs = global.db.data.msgs
-if (!text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' 𝙽𝙾 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂*`
+if (!text in msgs) throw `*⚠️ '${text}' NO REGISTRADO EN LA LISTA DE MENSAJES*`
 delete msgs[text]
-m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻𝙸𝙼𝙸𝙽𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾 𝙴𝙽 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 '${text}'*`)
+m.reply(`*✅ Eliminó con éxito la lista de mensajes llamada: '${text}'*`)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <text>')
 handler.tags = ['database']
