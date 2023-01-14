453cf2970ba93e5fed7abe4da9e5ea39dbbf05ac
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-13 22:00:03 -0400

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,36 +1,11 @@
-let handler = async (m, { conn, usedPrefix }) => {
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)]    
-let text = `
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
-*El número de mi creador es: wa.me/593959425714*
-*El número de mi creador 2 es' 𝙴𝚂 wa.me/5219992095479*`.trim()   
-let buttonMessage= {
-'document': { `github.com/DIEGO-OFC/ShadowBotV4-MD` },
-'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': 'https://github.com/DIEGO-OFC/ShadowBotV4-MD',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': 'El Mejor Bot de WhatsApp⁩',
-'body': wm,
-'thumbnail': imagen1,
-'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
-'caption': text,
-'footer': wm,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '🔰 MENU 🔰'}, type: 1}, 
-{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '📮 DONAR 📮'}, type: 1}],
-'headerType': 6 }
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
-handler.help = ['owner', 'creator']
+
+handler.help = ['owner']
 handler.tags = ['info']
-handler.command = /^(owner|creator|creador|propietario)$/i
+handler.command = ['owner', 'creator', 'creador', 'dueño', 'fgowner'] 
+
 export default handler
